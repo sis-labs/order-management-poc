b6be0e794c830261a10a3eeaa75c895ef3ef32f8
ollie314 <mehdi.lefebvre@gmail.com>
2019-12-01 09:27:11 +0100

    chore: add shipping service declaration
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

---
+++ include/shipping-service.h
@@ -0,0 +1,18 @@
+#ifndef SHIPPINGSERVICE_H
+#define SHIPPINGSERVICE_H
+
+
+class ShippingService
+{
+    public:
+        /** Default constructor */
+        ShippingService();
+        /** Default destructor */
+        virtual ~ShippingService();
+
+    protected:
+
+    private:
+};
+
+#endif // SHIPPINGSERVICE_H
---
+++ src/shipping-service.cpp
@@ -0,0 +1,11 @@
+#include "shipping-service.h"
+
+ShippingService::ShippingService()
+{
+    //ctor
+}
+
+ShippingService::~ShippingService()
+{
+    //dtor
+}
